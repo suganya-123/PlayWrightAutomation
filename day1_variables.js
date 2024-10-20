4d4b8c31e58dd34f8543d0b1c1eaec7d0dea7059
suganya-123 <indukumarsuganya06@gmail.com>
2024-10-20 17:19:49 +0530

    Add files via upload
    day1_variables committed

---
+++ day1_variables.js
@@ -0,0 +1,12 @@
+const browserVersion = "Chrome"
+console.log(browserVersion);
+function getBrowserVersion(browserVersion) {
+    if (browserVersion === 'Chrome') {
+        //var browserVersion = 'edge';
+        let browserVersion = 'edge';
+        console.log(browserVersion);
+    }
+    console.log(browserVersion);
+}
+//var browserVersion = 'chrome';
+getBrowserVersion(browserVersion);
